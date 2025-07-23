059fe0d328cdd37d6a043f18dd4626f83e5e5143
natrix-1987 <tom.krejcovsky@seznam.cz>
2025-07-23 16:23:08 +0200

    First full commit

---
+++ StartingProject/build/Desktop_Qt_6_7_2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appIntroToQML_FILE /home/mrbombastic/Software/Qt Academi/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/build/Desktop_Qt_6_7_2-Debug/appIntroToQML)
+set(__QT_DEPLOY_TARGET_appIntroToQML_TYPE EXECUTABLE)
